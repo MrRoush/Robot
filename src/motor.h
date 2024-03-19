9922071c03d7671a1929e2624d9d83c08ee6497d
Mike Roush <MRoush@kimberly.k12.wi.us>
2024-03-19 15:03:23 -0500

    Motor Header File

--- src/motor.h
+++ src/motor.h
@@ -1,11 +1,11 @@
 #ifndef __MOTOR_H	
 #define __MOTOR_H
 
-#define RMTR_BWD 10		//Right Motor Backward 
-#define RMTR_FWD 5		//Right Motor Forward
+#define RMTR_BWD 5		//Right Motor Backward 
+#define RMTR_FWD 10		//Right Motor Forward
 
-#define LMTR_BWD 9		//Left Motor Backward
-#define LMTR_FWD 8		//Left Motor Forward
+#define LMTR_BWD 8		//Left Motor Backward
+#define LMTR_FWD 9		//Left Motor Forward
 
 #define RENC_FWD 20		//Right Encoder Forward
 #define RENC_BWD 21		//Right Encoder Backward
